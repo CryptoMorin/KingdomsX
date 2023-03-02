9c32f0a47f5e6e9a5a82e6f240740ea55360c65e
NotTolerable <121141694+NotTolerable@users.noreply.github.com>
2023-03-02 17:25:25 -0500

    ProjectKorra compatibility fix
    Adds a potential compatibility fix by updating terms from both APIs

--- core/service/project-korra/src/main/java/org/kingdoms/services/ServiceProjectKorra.kt
+++ core/service/project-korra/src/main/java/org/kingdoms/services/ServiceProjectKorra.kt
@@ -1,42 +1,42 @@
 package org.kingdoms.services
 
 import com.projectkorra.projectkorra.ability.CoreAbility
-import com.projectkorra.projectkorra.configuration.ConfigManager
-import com.projectkorra.projectkorra.region.RegionProtectionBase
+import com.projectkorra.projectkorra.config.ConfigManager
+import com.projectkorra.projectkorra.region.RegionProtection
 import org.bukkit.Location
 import org.bukkit.entity.Player
 
 class ServiceProjectKorra(val handler: (player: Player, location: Location) -> Boolean) : Service {
     override fun enable() {
-        if (protection == null) protection = KingdomsRegionProtector()
+        if (protection == null) protection = KingdomsRegionProtection()
     }
 
     fun registerConfig() {
-        // https://github.com/ProjectKorra/ProjectKorra/blob/97c28fab4a8e1007ba0deabbe2afc9886efefce6/src/com/projectkorra/projectkorra/configuration/ConfigManager.java
+        // https://github.com/ProjectKorra/ProjectKorra/blob/master/src/main/java/com/projectkorra/projectkorra/config/ConfigManager.java
         val config = ConfigManager.defaultConfig.get()
         config.addDefault("Properties.RegionProtection.Kingdoms.Respect", true)
         config.addDefault("Properties.RegionProtection.Kingdoms.ProtectDuringInvasions", false)
     }
 
     companion object {
-        private var protection: KingdomsRegionProtector? = null
+        private var protection: KingdomsRegionProtection? = null
 
-        // https://github.com/ProjectKorra/ProjectKorra/blob/e2013bc700b52f72f2963fd2d6775a6923679616/src/com/projectkorra/projectkorra/GeneralMethods.java#L1551
+        // https://github.com/ProjectKorra/ProjectKorra/blob/master/src/main/java/com/projectkorra/projectkorra/GeneralMethods.java#L1551
         @JvmStatic
         fun isEnabled(): Boolean =
             ConfigManager.defaultConfig.get().getBoolean("Properties.RegionProtection.Kingdoms.Respect")
 
         @JvmStatic
         fun protectDuringInvasions(): Boolean =
-            ConfigManager.getConfig().getBoolean("Properties.RegionProtection.Kingdoms.ProtectDuringInvasions")
+            ConfigManager.defaultConfig.get().getBoolean("Properties.RegionProtection.Kingdoms.ProtectDuringInvasions")
     }
 
-    inner class KingdomsRegionProtector : RegionProtectionBase("Kingdoms", "Kingdoms.Respect") {
+    inner class KingdomsRegionProtection : RegionProtection("Kingdoms", "Kingdoms.Respect") {
         override fun isRegionProtectedReal(
             player: Player, location: Location, ability: CoreAbility?,
             harmless: Boolean, igniteAbility: Boolean, explosiveAbility: Boolean
         ): Boolean {
-            // https://github.com/ProjectKorra/ProjectKorra/blob/3ee5c834f96d860be1792b38b2ded2d140dd62bf/src/com/projectkorra/projectkorra/region/Kingdoms.java
+            // https://github.com/ProjectKorra/ProjectKorra/blob/master/src/main/java/com/projectkorra/projectkorra/region/ProtectionManager.java
             return handler(player, location)
         }
     }
